5ee038fae37db7e7c6e3827a4d44f7628ad9501e
mallikanicole <malmichael23@gmail.com>
2025-01-24 22:00:52 -0500

    test task 2

--- cc_2.js
+++ cc_2.js
@@ -5,3 +5,7 @@ console.log("New Product List:",products)
 let removed=products.pop();//removing knife from product list
 console.log("Product being removed:", removed)
 console.log("Updated Product List:", products)
+let scores=[85,98,67,77,52];//original test scores
+console.log("Student Scores:", scores)
+scores.splice(1,1,79);//changing 98 to 79
+console.log("New Scores:", scores)
