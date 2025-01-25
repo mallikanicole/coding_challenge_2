885c53b619be0bd4c76f7758918ce8e0149e8db3
mallikanicole <malmichael23@gmail.com>
2025-01-24 21:45:49 -0500

    test task 1

--- cc_2.js
+++ cc_2.js
@@ -1,1 +1,6 @@
-let products=["bowl","plate","fork","spoon","knife"]
+let products=["bowl","plate","fork","spoon","knife"];//original product list
+console.log("Products:", products)
+products.push(mug);//adding new product
+let removed=products,pop()
+console.log(removed)
+
