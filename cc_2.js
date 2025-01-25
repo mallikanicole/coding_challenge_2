39db044d0dfa1d20f009d303d69c9af597c185ba
mallikanicole <malmichael23@gmail.com>
2025-01-24 21:54:40 -0500

    Task 1-Store Inventory

--- cc_2.js
+++ cc_2.js
@@ -3,5 +3,5 @@ console.log("Products:", products)
 products.unshift("mug");//adding new product
 console.log("New Product List:",products)
 let removed=products.pop();//removing knife from product list
-console.log(removed)
+console.log("Product being removed:", removed)
 console.log("Updated Product List:", products)
