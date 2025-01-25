9229fd6735d397d9e64ae53e698396ef52d4d33e
mallikanicole <malmichael23@gmail.com>
2025-01-24 21:53:18 -0500

    test task1

--- cc_2.js
+++ cc_2.js
@@ -2,6 +2,6 @@ let products=["bowl","plate","fork","spoon","knife"];//original product list
 console.log("Products:", products)
 products.unshift("mug");//adding new product
 console.log("New Product List:",products)
-let removed=products.pop();//removing mug from product list
+let removed=products.pop();//removing knife from product list
 console.log(removed)
 console.log("Updated Product List:", products)
